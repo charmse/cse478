2fd1da1bfdd8bec9d40786d09189df9c8af26a62
charmse <caleharms@protonmail.ch>
2018-07-02 18:14:41 -0500

    Add model.ipynb

--- assignments/3/scipy_ec.py
+++ assignments/3/scipy_ec.py
@@ -1,4 +1,18 @@
 from scipy.optimize import minimize
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+%run 'model.ipynb'
+
+#Load MNIST training and testing images and labels
+x_train = np.load("../data/mnist_tr_x.npy")
+y_train = one_hot(np.load("../data/mnist_tr_y.npy"))
+x_test = np.load("../data/mnist_te_x.npy")
+y_test = one_hot(np.load("../data/mnist_te_y.npy"))
+# visualize some of the data
+print("A label looks like this: " + str(y_train[0]))
+print("And an image looks like this:")
+imgplot = plt.imshow(x_train[0].reshape((28,28)))
 
 def cost(theta,X,Y):
     theta = theta.reshape((784,10))
